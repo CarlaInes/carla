019f2cd8c18b16090af56b74d79266c991327dce
kilian1322 <kilian.huellen@gmx.de>
2023-12-14 20:36:01 +0100

    changes to scenario_manager so far, but not running for arbitrary scenarios yet

--- runnerTool/srunner/scenariomanager/scenario_manager.py
+++ runnerTool/srunner/scenariomanager/scenario_manager.py
@@ -11,9 +11,15 @@ It must not be modified and is for reference only!
 """
 
 from __future__ import print_function
+from cmath import sin, sqrt
+from itertools import product
+import logging
+import math
+import random
 import sys
 import time
 import carla
+import pandas as pd
 import py_trees
 
 from srunner.autoagents.agent_wrapper import AgentWrapper
@@ -175,6 +181,105 @@ class ScenarioManager(object):
 
         spectator.set_transform(transform) 
     ####################
+            
+    ####################################################################################################################################
+    # Functions to get information about the ego vehicle
+    def get_speed(self,ego_vehicle):
+        # Get the velocity of the ego vehicle
+        velocity = ego_vehicle.get_velocity()
+        speed = velocity.length()
+        return speed #in m/s
+
+    def get_acceleration(self,speed, previous_speed, current_time, previous_time):
+        time_interval = (current_time - previous_time) / 1_000_000_000
+        acceleration = (speed - previous_speed) / time_interval  # Change in speed during one time interval
+        return acceleration
+
+    def get_steering_angle(self,ego_vehicle):
+        """
+        steering_angle < 0: driving left
+        steering_angle > 0: driving right
+        """
+        control = ego_vehicle.get_control()
+
+        steering_angle = control.steer * 180 #angle in degree, control.steer in [-1.0, 1.0]
+        #steering_angle = steering_angle * (math.pi / 360) #angle in radians
+        return steering_angle
+
+    def get_lateral_acceleration(self,ego_vehicle, current_time, previous_time, previous_yaw, speed):
+        time_interval = (current_time - previous_time) / 1_000_000_000
+        # Get the lateral rotation (yaw) of the ego vehicle
+        current_yaw = ego_vehicle.get_transform().rotation.yaw
+        delta_yaw = math.radians(current_yaw - previous_yaw)
+
+        lateral_acceleration = (speed * delta_yaw) / time_interval
+
+        return lateral_acceleration, current_yaw
+
+
+    def get_dist_to_lane_center(self,ego_vehicle, world):
+        ego_location = ego_vehicle.get_location()
+        waypoint = world.get_map().get_waypoint(ego_location)
+        lane_location = waypoint.transform.location
+        dist_to_lane_center = math.sqrt((lane_location.x - ego_location.x) **2 + (lane_location.y - ego_location.y) ** 2)
+
+        return dist_to_lane_center
+
+
+    def get_speed_of_vehicle_ahead(self, ego_waypoint, world, max_distance=10):
+        #camera_data = camera_sensor.listen()
+        map = world.get_map()
+        actor_locations = [(self.get_speed(actor), map.get_waypoint(actor.get_location()).transform.location) for actor in world.get_actors()]
+        for i in range(1, max_distance + 1):
+            next_waypoint = ego_waypoint.next(i)[0]
+            for actor_speed, actor_location in actor_locations:
+                if actor_location.distance(next_waypoint.transform.location) < 1:
+                    return actor_speed
+                
+        return None
+                
+
+    def get_speed_of_vehicle_ahead_efficient(self, ego_waypoint, max_distance=10):
+        #camera_data = camera_sensor.listen()
+        actor_locations = [(self.get_speed(actor), map.get_waypoint(actor.get_location()).transform.location) for actor in world.get_actors()]
+
+        for i, actor_speed, actor_location in product(range(1, max_distance + 1), *zip(*actor_locations)):
+            next_waypoint = ego_waypoint.next(i)[0]
+            distance = actor_location.distance(next_waypoint.transform.location)
+            if distance < 1:
+                return actor_speed
+
+        # Return a default value if no valid speed is found
+        return None
+
+    # Function to get the curvature of the road at the ego vehicle's current location in degrees
+    def get_curvature_at_location(self, vehicle_location, world):
+        map = world.get_map()
+        # Find the nearest waypoint corresponding to the vehicle's location
+        waypoint = map.get_waypoint(vehicle_location)
+        # Get the curvature at the waypoint
+        curvature_coeff = self.get_curvature_coeff(waypoint, 1)
+
+        return curvature_coeff
+
+
+    def get_curvature_coeff(self,ego_waypoint, route_distance):
+        previous_waypoint = ego_waypoint.previous(route_distance)[0]
+        next_waypoint = ego_waypoint.next(route_distance)[0]
+        _transform = next_waypoint.transform
+        _location, _rotation  = _transform.location, _transform.rotation
+        x1, y1 = _location.x, _location.y
+        yaw1 = _rotation.yaw
+
+        _transform = previous_waypoint.transform
+        _location, _rotation  = _transform.location, _transform.rotation
+        x2, y2 = _location.x, _location.y
+        yaw2 = _rotation.yaw
+
+        c = 2*sin(math.radians((yaw1-yaw2)/2)) / sqrt((x1-x2)**2 + (y1-y2)**2)
+        return c
+    ####################################################################################################################################
+        
 
 
     def run_scenario(self):
@@ -189,6 +294,36 @@ class ScenarioManager(object):
         self._watchdog.start()
         self._running = True
 
+        # Create an empty list to store the data as dictionaries
+        data_list = []
+
+        previous_speed = 0  # Store the previous speed value in m/s
+        previous_yaw = 0  # Store the previous yaw angle value
+        leading_vehicle = None  # Variable to store the leading vehicle reference
+        previous_time = time.time_ns()
+
+        world = CarlaDataProvider.get_world()          
+
+        time_interval = 1.0 #for acceleration, in s
+        ego_vehicle_bp = world.get_blueprint_library().find('vehicle.audi.a2')
+
+
+        spawn_points = world.get_map().get_spawn_points()
+        number_of_spawn_points = len(spawn_points)
+
+        if 0 < number_of_spawn_points:
+            random.shuffle(spawn_points)
+            ego_transform = spawn_points[0]
+            ego_vehicle = world.spawn_actor(ego_vehicle_bp,ego_transform)
+            print('\nEgo is spawned')
+        else: 
+            logging.warning('Could not find any spawn points')
+        print("initialization successful")
+
+
+
+
+
         #k = 0
         while self._running:
             timestamp = None
@@ -200,9 +335,45 @@ class ScenarioManager(object):
                         #self.reset_camera(world.get_actor(self.ego_vehicles[0].id),world.get_spectator())
 
                 snapshot = world.get_snapshot()
-
+                print("start of data collection")
                 #TODO: adding our (or at least my code) here
+                 #returns the current speed of the ego vehicle in m/s
+                speed = self.get_speed(ego_vehicle)
+                current_time = time.time_ns()
+
+                acceleration = self.get_acceleration(speed, previous_speed, current_time, previous_time)
+
+                steering_angle = self.get_steering_angle(ego_vehicle) #TODO angle in degree or radians?
+
+                lateral_acceleration, current_yaw = self.get_lateral_acceleration(ego_vehicle, current_time, previous_time, previous_yaw, speed)
+
+                dist_to_lane_center = self.get_dist_to_lane_center(ego_vehicle, world) #in m #TODO needs to be tested with manual car
+
+                ego_location = ego_vehicle.get_location()
+                waypoint = world.get_map().get_waypoint(ego_location)
+                speed_vehicle_ahead = self.get_speed_of_vehicle_ahead(waypoint, world) 
+
+                # Get and print the curvature at the ego vehicle's location in degrees per meter
+                curvature_degrees_per_meter = self.get_curvature_at_location(ego_vehicle.get_location(), world) 
+
+                #print({'Speed (m/s)': speed, '\nAcceleration (m/s^2)': acceleration, '\nSteering Angle' : steering_angle, '\nLateral Acceleration' : lateral_acceleration})
+                #print('\nCurvature at Ego Vehicle Location (Degrees/m): ' + str(curvature_degrees_per_meter))
+            
+
+
+                # Append the data as a dictionary to the list
+                data_list.append({'Speed (m/s)\t\t': speed, 'Acceleration (m/s^2)\t\t': acceleration, 'Steering Angle\t\t' : steering_angle,\
+                                'Lateral Acceleration (m/s^2)\t\t' : lateral_acceleration, 'Distance to lane center (m)\t\t' : dist_to_lane_center,\
+                                    'Speed of vehicle ahead (m/s)\t\t' : speed_vehicle_ahead,\
+                                    'Road Curvature at Ego Vehicle Location (Degrees/m)\t\t' : curvature_degrees_per_meter})
                 
+                print(speed)
+                
+
+                previous_speed = speed  # Update the previous speed value
+                previous_time = current_time
+                previous_yaw = current_yaw  # Update the previous yaw angle value
+                print("end of data collection")
 
                 if snapshot:
                     timestamp = snapshot.timestamp
@@ -211,6 +382,13 @@ class ScenarioManager(object):
             #k+=1
 
         self.cleanup()
+
+        # Convert the list of dictionaries to a DataFrame
+        data_df = pd.DataFrame(data_list)
+
+        #Save data from the dataframe in a csv file
+        filename = 'test_data/' + time.strftime("%Y-%m-%d_%H-%M-%S") + '.csv'
+        data_df.to_csv(filename, index=False)
 
         self.end_system_time = time.time()
         end_game_time = GameTime.get_time()
